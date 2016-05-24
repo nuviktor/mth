adcffea2e2dbda07dd66f8d9e92b5fa50fc4b5a9
Viktor Numerov <nuviktor@yahoo.com>
2016-05-24 20:43:43 +0100

    Improved calculations and added calculation of host utilising the most bandwidth

--- src/mth.c
+++ src/mth.c
@@ -1,11 +1,12 @@
-#include <net/ethernet.h>
+#include <arpa/inet.h>
+#include <byteswap.h>
 #include <linux/if_ether.h>
+#include <net/ethernet.h>
 #include <netinet/ether.h>
 #include <netinet/ip.h>
+#include <pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pcap.h>
-#include <byteswap.h>
 
 #define NETWORK 0x0001a8c0 // 192.168.1.0
 #define NETMASK 0x00ffffff // 255.255.255.0
@@ -18,24 +19,29 @@ void
 packet_cb(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
 	extern unsigned long ips[];
-        const struct iphdr *ip_ptr;
+	const struct iphdr *ip_ptr;
 	u_int32_t host;
 
 	ip_ptr = (struct iphdr*)(packet + ETH_HLEN);
 
 	if ((ip_ptr->daddr & NETMASK) == NETWORK) {
 		host = ip_ptr->daddr & ~NETMASK;
-		ips[__bswap_32(host)] += header->len;
+		ips[ntohl(host)] += header->len;
 	}
-	else if ((ip_ptr->saddr & NETMASK) == NETWORK) {
+
+	if ((ip_ptr->saddr & NETMASK) == NETWORK) {
 		host = ip_ptr->saddr & ~NETMASK;
-		ips[__bswap_32(host)] += header->len;
+		ips[ntohl(host)] += header->len;
 	}
 }
 
 int main(int argc, char *argv[])
 {
 	int i;
+	int mth = 1;
+
+	u_int32_t ip;
+	char ipstr[INET_ADDRSTRLEN];
 
         char *dev = argv[1];
         char errbuf[PCAP_ERRBUF_SIZE];
@@ -78,9 +84,15 @@ int main(int argc, char *argv[])
         pcap_freecode(&fp);
         pcap_close(handle);
 
-	for (i = 0; i < NHOSTS; i++)
-		if (ips[i] != 0)
-			printf("Host %d: %lu bytes\n", i, ips[i]);
+	// Ignore the network and broadcast addresses
+	for (i = 1; i < (NHOSTS-1); i++)
+		if (ips[i] > ips[mth])
+			mth = i;
+
+	ip = htonl((u_int32_t) mth) | NETWORK;
+	inet_ntop(AF_INET, &ip, ipstr, INET_ADDRSTRLEN);
+
+	printf("%s\n", ipstr);
 
         return EXIT_SUCCESS;
 }
