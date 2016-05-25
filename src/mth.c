142949c5944fdf685f49526f87de87700ef277af
Viktor Numerov <nuviktor@yahoo.com>
2016-05-25 11:23:20 +0100

    Made identation more consistent

--- src/mth.c
+++ src/mth.c
@@ -43,46 +43,46 @@ int main(int argc, char *argv[])
 	u_int32_t ip;
 	char ipstr[INET_ADDRSTRLEN];
 
-        char *dev = argv[1];
-        char errbuf[PCAP_ERRBUF_SIZE];
-        pcap_t *handle;
+	char *dev = argv[1];
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t *handle;
 
-        struct bpf_program fp;
-        char filter_exp[] = "ip";
+	struct bpf_program fp;
+	char filter_exp[] = "ip";
 
-        if (argc < 2) {
-                fprintf(stderr, "No argument supplied\n");
-                exit(EXIT_FAILURE);
-        }
+	if (argc < 2) {
+		fprintf(stderr, "No argument supplied\n");
+		exit(EXIT_FAILURE);
+	}
 
-        handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-        if (handle == NULL) {
-                fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-                exit(EXIT_FAILURE);
-        }
+	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == NULL) {
+		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		exit(EXIT_FAILURE);
+	}
 
-        if (pcap_datalink(handle) != DLT_EN10MB) {
-                fprintf(stderr, "Device %s doesn't provide Ethernet headers\n", dev);
-                exit(EXIT_FAILURE);
-        }
+	if (pcap_datalink(handle) != DLT_EN10MB) {
+		fprintf(stderr, "Device %s doesn't provide Ethernet headers\n", dev);
+		exit(EXIT_FAILURE);
+	}
 
-        if (pcap_compile(handle, &fp, filter_exp, 0, PCAP_NETMASK_UNKNOWN) == -1) {
-                fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-                exit(EXIT_FAILURE);
-        }
+	if (pcap_compile(handle, &fp, filter_exp, 0, PCAP_NETMASK_UNKNOWN) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		exit(EXIT_FAILURE);
+	}
 
-        if (pcap_setfilter(handle, &fp) == -1) {
-                fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-                exit(EXIT_FAILURE);
-        }
+	if (pcap_setfilter(handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		exit(EXIT_FAILURE);
+	}
 
 	for (i = 0; i < NHOSTS; i++)
 		ips[i] = 0;
 
-        pcap_loop(handle, 512, packet_cb, NULL);
+	pcap_loop(handle, 512, packet_cb, NULL);
 
-        pcap_freecode(&fp);
-        pcap_close(handle);
+	pcap_freecode(&fp);
+	pcap_close(handle);
 
 	// Ignore the network and broadcast addresses
 	for (i = 1; i < (NHOSTS-1); i++)
@@ -94,5 +94,5 @@ int main(int argc, char *argv[])
 
 	printf("%s\n", ipstr);
 
-        return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
